1c5e2ec265d776d0162c98d6b40aae6aaafd6d01
Hrittik-Chatterjee <@hrittik0chatterjee@gmail.com>
2022-09-29 20:48:27 +0600

    fake data added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Exercises from './components/Exercises/Exercises';
 import Header from './components/Header/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div >
     
        <Header></Header>
+       <Exercises></Exercises>
      
     </div>
   );
---
+++ src/components/Exercises/Exercises.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import"./Exercises.css"
+
+const Exercises = () => {
+    return (
+        <div className='exercise-container'>
+            <div className='exercise-list-container'>
+            
+            </div>
+            <div className='time-calculate-container'>
+    
+            </div>
+        </div>
+    );
+};
+
+export default Exercises;
