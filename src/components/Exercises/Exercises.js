c21f8bf0e7725ea6d07d5c7b7c3ee6227af06665
Hrittik-Chatterjee <@hrittik0chatterjee@gmail.com>
2022-09-30 17:00:50 +0600

    time added succesfully

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import"./Exercises.css"
 
 const Exercises = () => {
+    const [exercises, setExercises] =useState([])
+    useEffect( ()=>{
+        fetch('exercises.json')
+        .then(res=> res.json())
+        .then(data=> setExercises(data))
+    }, [])
     return (
         <div className='exercise-container'>
             <div className='exercise-list-container'>
-            
+                <h2>this is for exercise list:{exercises.length}</h2>
             </div>
             <div className='time-calculate-container'>
     
